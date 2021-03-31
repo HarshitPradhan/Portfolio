09bec4aaddbb28f33da042458217a5a648478a9e
HarshitPradhan <59720787+HarshitPradhan@users.noreply.github.com>
2021-03-31 20:48:01 +0530

    Portfolio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { BackgroundComponent } from "./background/background.component"
+import { LoginComponent } from "./auth/login/login.component";
+import { RegisterComponent } from "./auth/register/register.component";
+const routes: Routes = [
+  {
+    path : 'Home', component : BackgroundComponent
+  },
+ {
+   path : 'Login', component : LoginComponent
+ },
+ {
+  path : 'Register', component : RegisterComponent
+},
+{
+  path : '', component : BackgroundComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
+import { BackgroundComponent } from './background/background.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BackgroundComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,BackgroundComponent]
 })
 export class AppModule { }
